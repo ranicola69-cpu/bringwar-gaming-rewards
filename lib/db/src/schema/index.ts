f6c28d13d1464f6dc929ce95a4ded85da9632eb2
ranicola69-cpu <ranicola69@gmail.com>
2026-03-25 10:30:53 -0400

    Export postback_logs and daily_rewards schema

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -2,3 +2,5 @@ export * from "./users";
 export * from "./offers";
 export * from "./completions";
 export * from "./withdrawals";
+export * from "./postback_logs";
+export * from "./daily_rewards";
