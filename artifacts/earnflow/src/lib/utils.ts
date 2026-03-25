485d29cb7869a3958d01d88c08af55894b7de13a
ranicola69-cpu <ranicola69@gmail.com>
2026-03-25 09:31:34 -0400

    Add artifacts/earnflow/src/lib/utils.ts

---
+++ artifacts/earnflow/src/lib/utils.ts
@@ -0,0 +1,17 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
+}
+
+export function formatPoints(points: number) {
+  return new Intl.NumberFormat("en-US").format(points);
+}
